029b89a60d2a72c46cc1453cae7e903fe05a79ad
abhirampattarkine <abhiram.vjti@gmail.com>
2025-11-28 23:16:39 -0500

    Added code for check and add functionality

--- main.go
+++ main.go
@@ -21,17 +21,15 @@ type Queue struct {
 
 type Cache struct {
 	queue Queue
-	hash  Hash
+	hash  map[string]*Node
 }
-
-type Hash map[string]*Node
 
 // Functions and Methods to operate the cache.
 
 func NewCache(maxLength int) Cache {
 	return Cache{
 		queue: NewQueue(maxLength),
-		hash:  Hash{},
+		hash:  make(map[string]*Node),
 	}
 }
 
@@ -45,9 +43,47 @@ func NewQueue(maxLength int) Queue {
 	return queue
 }
 
-func (c Cache) Add(item string) {
-	// Check if the item is already present in the Cache
-	_, ok := Hash[item]
+func (c Cache) Add(word string) {
+	/*
+		- Check the length of the cache. If the length is full then we have to first remove.
+		- Else we have to add at the first.
+		- Also, check if this is the very first element in the queue. In that case add directly.
+		- Also update the hashmap data structure each time we add or remove the items.
+	*/
+
+	// Queue is empty. Create a new node.
+	newNode := &Node{
+		Left:  nil,
+		Right: nil,
+		Val:   word,
+	}
+	c.hash[word] = newNode
+	// If the maxLength has not reached
+	if c.queue.Length < c.queue.MaxLength {
+
+		// If the item is first item in the queue
+		if c.queue.Length == 0 {
+			c.queue.Head = newNode
+			c.queue.Length += 1
+			c.queue.Tail = newNode
+			return
+		} else {
+			newNode.Right = c.queue.Head
+			c.queue.Head.Left = newNode
+			c.queue.Head = newNode
+			c.queue.Length += 1
+		}
+	} else {
+		// Remove the last Item
+		c.Remove()
+		// Remove the item from hashmap
+		delete(c.hash, word)
+		// Add the item to the front
+		newNode.Right = c.queue.Head
+		c.queue.Head.Left = newNode
+		c.queue.Head = newNode
+		c.queue.Length += 1
+	}
 
 }
 
@@ -55,11 +91,34 @@ func (c Cache) Remove() {
 
 }
 
-func (c Cache) Adjust() {
+func (c Cache) Adjust(word string) {
+
+	if c.queue.Head == c.queue.Tail {
+		return
+	} else {
+		// Create a temp node to point to the current node
+		tempNode := c.hash[word]
+		// Move the things around
+		tempNode.Left.Right = tempNode.Right
+		tempNode.Right = c.queue.Head
+		tempNode.Left = nil
+		c.queue.Head.Left = tempNode
+		c.queue.Head = tempNode
+	}
 
 }
 
 func (c Cache) Check(word string) {
+
+	// Find if the item is present in the cache
+	_, ok := c.hash[word]
+
+	// if Okay call adjust else call add
+	if ok {
+		c.Adjust(word)
+	} else {
+		c.Add(word)
+	}
 
 }
 
